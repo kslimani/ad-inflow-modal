9b08a75d4e6d118a28c87ff6548087649cf6fc7e
Karim Slimani <karim@playmedia.fr>
2018-12-20 13:48:45 +0100

    feat: add prevent method to body locker

--- src/body-locker.js
+++ src/body-locker.js
@@ -4,24 +4,55 @@ import { addOneClass, removeClass } from './dom'
 
 export default class BodyLocker {
   constructor() {
-    this._e = 'touchmove'
     this._c = 'ad-inflow-body'
+    this._el = 'touchmove'
+    this._ep = ['touchstart', 'wheel', 'mousedown']
+    this._lock = (e) => {
+      e.preventDefault()
+    }
     this._prevent = (e) => {
       e.preventDefault()
+
+      if (this._p) {
+        return
+      }
+
+      this._p = true
+      this._next()
+
+      this._ep.forEach((e) => {
+        document.body.removeEventListener(e, this._prevent, {
+          capture: true,
+          passive: false,
+          once: true,
+        })
+      })
     }
   }
 
   lock() {
     addOneClass(document.body, this._c)
-    document.body.addEventListener(this._e, this._prevent, {
+    document.body.addEventListener(this._el, this._lock, {
       passive: false,
     })
   }
 
   unlock() {
     removeClass(document.body, this._c)
-    document.body.removeEventListener(this._e, this._prevent, {
+    document.body.removeEventListener(this._el, this._lock, {
       passive: false,
+    })
+  }
+
+  prevent(next) {
+    this._p = false
+    this._next = next
+    this._ep.forEach((e) => {
+      document.body.addEventListener(e, this._prevent, {
+        capture: true,
+        passive: false,
+        once: true,
+      })
     })
   }
 }
