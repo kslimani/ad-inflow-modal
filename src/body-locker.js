734c98c7078d7b69d928e62ccfb963a3c35ffd49
Karim Slimani <karim@playmedia.fr>
2018-12-21 15:06:48 +0100

    fix: enhance body locker event bindings

--- src/body-locker.js
+++ src/body-locker.js
@@ -2,15 +2,34 @@
 
 import { addOneClass, removeClass } from './dom'
 
+let isiPad = navigator.userAgent.match(/iPad/i) != null
+
 export default class BodyLocker {
   constructor() {
     this._c = 'ad-inflow-body'
     this._el = 'touchmove'
-    this._ep = ['touchstart', 'wheel', 'mousedown']
+    this._ep = [
+      'touch',
+      'touchstart',
+      'touchmove',
+      'touchcancel',
+    ]
+    this._en = [
+      'touchend',
+      'mousedown',
+    ]
+
+    if (isiPad) {
+      this._en.unshift('touchstart')
+    }
+
     this._lock = (e) => {
       e.preventDefault()
     }
     this._prevent = (e) => {
+      e.preventDefault()
+    }
+    this._prevented = (e) => {
       e.preventDefault()
 
       if (this._p) {
@@ -20,6 +39,13 @@ export default class BodyLocker {
       this._p = true
       this._next()
 
+      this._en.forEach((e) => {
+        document.body.removeEventListener(e, this._prevented, {
+          capture: true,
+          passive: false,
+          once: true,
+        })
+      })
       this._ep.forEach((e) => {
         document.body.removeEventListener(e, this._prevent, {
           capture: true,
@@ -47,6 +73,13 @@ export default class BodyLocker {
   prevent(next) {
     this._p = false
     this._next = next
+    this._en.forEach((e) => {
+      document.body.addEventListener(e, this._prevented, {
+        capture: true,
+        passive: false,
+        once: true,
+      })
+    })
     this._ep.forEach((e) => {
       document.body.addEventListener(e, this._prevent, {
         capture: true,
