86eb1e875bdcaef31a2acf2eddf32a168c1673b6
Karim Slimani <karim@playmedia.fr>
2020-03-02 11:11:06 +0100

    [0.2.5]

--- dist/ad-inflow-modal.js
+++ dist/ad-inflow-modal.js
@@ -113,7 +113,7 @@ exports.toggleClass = toggleClass;
 exports.attr = attr;
 exports.make = make;
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 // dom.js
 // Convert value to string
@@ -323,9 +323,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 var isMobile = (0, _detectMobile["default"])();
 
-var AdInflowModal =
-/*#__PURE__*/
-function () {
+var AdInflowModal = /*#__PURE__*/function () {
   function AdInflowModal(options) {
     var _this = this;
 
@@ -341,7 +339,7 @@ function () {
       _this.close();
     };
 
-    this._onPlayIntent = function (e) {
+    this._onPlayIntent = function () {
       _this._hidePlayOverlay();
 
       _this._adPlayer.play();
@@ -495,7 +493,8 @@ function () {
 
       try {
         console.log('ad-inflow-modal:', e);
-      } catch (err) {}
+      } catch (err) {} // eslint-disable-line no-empty
+
     }
   }, {
     key: "_makeAdPlayer",
@@ -532,7 +531,9 @@ function () {
       if (isMobile) {
         this._open(false);
       } else {
-        (0, _canAutoplay["default"])(function (result, error) {
+        (0, _canAutoplay["default"])(function (result)
+        /*error*/
+        {
           _this4._open(result);
         }, timeout);
       }
@@ -570,17 +571,17 @@ function () {
         });
       }
 
-      this._adPlayer.on('started', function (o) {
+      this._adPlayer.on('started', function () {
         _this5._showCloseButton(_this5._o.closeButtonDelay);
       });
 
-      this._adPlayer.on('ad_end', function (o) {
+      this._adPlayer.on('ad_end', function () {
         _this5._close();
       });
 
       if (autoplay || this._o.openOnInteractionIfNoAutoplay) {
         // Modal will show up on "ad begin" ad player event
-        this._adPlayer.on('ad_begin', function (o) {
+        this._adPlayer.on('ad_begin', function () {
           _this5._show();
 
           _this5._body.lock();
@@ -681,7 +682,6 @@ function () {
 }();
 
 exports["default"] = AdInflowModal;
-module.exports = exports.default;
 
 /***/ }),
 /* 3 */
@@ -705,9 +705,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 var isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
-var BodyLocker =
-/*#__PURE__*/
-function () {
+var BodyLocker = /*#__PURE__*/function () {
   function BodyLocker() {
     var _this = this;
 
@@ -806,7 +804,6 @@ function () {
 }();
 
 exports["default"] = BodyLocker;
-module.exports = exports.default;
 
 /***/ }),
 /* 4 */
@@ -824,10 +821,10 @@ exports["default"] = detectMobile;
 // Regular Expression: http://detectmobilebrowsers.com/
 function detectMobile() {
   var match = navigator.userAgent || navigator.vendor || window.opera;
+  /* eslint-disable no-useless-escape */
+
   return !!(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(match) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(match.substr(0, 4)));
 }
-
-module.exports = exports.default;
 
 /***/ }),
 /* 5 */
@@ -880,8 +877,6 @@ function _default(cb) {
     canAutoplay(true);
   }
 }
-
-module.exports = exports.default;
 
 /***/ }),
 /* 6 */
@@ -1742,10 +1737,8 @@ function _default(options, cb) {
     }
 
     cb(new _imaPlayer["default"](options), null);
-  }, (0, _utils.makeNum)(options.timemout, 6000), !!options.debug);
+  }, (0, _utils.makeNum)(options.timeout, 6000), !!options.debug);
 }
-
-module.exports = exports.default;
 
 /***/ }),
 /* 41 */
@@ -1794,8 +1787,6 @@ function _default(cb) {
     }, timeout);
   }
 }
-
-module.exports = exports.default;
 
 /***/ }),
 /* 42 */
@@ -2162,9 +2153,8 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-var ImaPlayer =
-/*#__PURE__*/
-function () {
+/* global google */
+var ImaPlayer = /*#__PURE__*/function () {
   function ImaPlayer(options) {
     _classCallCheck(this, ImaPlayer);
 
@@ -2642,7 +2632,6 @@ function () {
 }();
 
 exports["default"] = ImaPlayer;
-module.exports = exports.default;
 
 /***/ }),
 /* 59 */
@@ -2741,9 +2730,7 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 // observable.js
-var Observable =
-/*#__PURE__*/
-function () {
+var Observable = /*#__PURE__*/function () {
   function Observable() {
     _classCallCheck(this, Observable);
 
@@ -2800,7 +2787,6 @@ function () {
 }();
 
 exports["default"] = Observable;
-module.exports = exports.default;
 
 /***/ }),
 /* 63 */
@@ -3193,7 +3179,7 @@ module.exports = {
 
 
 /***/ })
-/******/ ]);
+/******/ ])["default"];
 });
 
 /***/ }),
@@ -3215,5 +3201,5 @@ module.exports = "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmln
 module.exports = "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\" viewBox=\"0 0 510 510\" style=\"enable-background:new 0 0 510 510;\" xml:space=\"preserve\"><path class=\"ad-inflow-modal-overlay-play \" d=\"M204,369.75L357,255L204,140.25V369.75z M255,0C114.75,0,0,114.75,0,255s114.75,255,255,255s255-114.75,255-255 S395.25,0,255,0z M255,459c-112.2,0-204-91.8-204-204S142.8,51,255,51s204,91.8,204,204S367.2,459,255,459z\" fill=\"#FFFFFF\"></path></svg>"
 
 /***/ })
-/******/ ]);
+/******/ ])["default"];
 });
